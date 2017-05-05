963c238772f920f2838ee297059f36a47ddf0dc4
David Bebb <davidbebb@gmail.com>
2017-05-05 08:22:37 +0100

    simpler graphql endpoint, with easier access to post graphql middleware

--- app.js
+++ app.js
@@ -1,20 +1,38 @@
-var yn = require('yn');
 require('dotenv-safe').load();
-var useGraphiql = yn(process.env.GRAPHIQL)
 
 var express = require('express');
 var logger = require('morgan');
-var graphqlHTTP = require('express-graphql');
-var app = express();
+var {graphql} = require('graphql')
+var bodyParser = require('body-parser');
+
 
 var schema = require ('./graphql/testSchema');
 
+var app = express();
+
 app.use(logger('dev'));
 
-app.use('/graphql', graphqlHTTP({
-  schema: schema,
-  graphiql: useGraphiql
-}));
+app.use(bodyParser.raw(
+  { type: 'application/graphql' }
+));
+
+app.use(function (req, res, next) {
+  req.graphql = {};
+  req.graphql.query = req.body.toString('utf8');
+  next();
+})
+
+app.post('/graphql', function (req, res, next){
+  graphql(schema, req.graphql.query).then(function(result){
+    req.graphql.result = result;
+    next();
+  });
+});
+
+
+app.use( function (req, res, next) {
+  res.send(req.graphql.result);
+})
 
 app.listen(process.env.PORT);
 console.log('Running a GraphQL API server at localhost:' + process.env.PORT+ '/graphql');
