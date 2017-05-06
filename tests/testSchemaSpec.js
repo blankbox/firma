06f5e99afea722676622d5a095d2481897543159
David Bebb <davidbebb@gmail.com>
2017-05-06 18:08:09 +0100

    added basic enpoint tests as example usage

---
+++ tests/testSchemaSpec.js
@@ -0,0 +1,61 @@
+require('dotenv-safe').load('../.env');
+
+
+var request = require('request');
+var assert = require('assert');
+
+
+var url = 'http://localhost:' + process.env.PORT + '/graphql';
+
+var requestContent = {
+  method:'POST',
+  uri: url,
+  headers:{
+    'Content-Type':'application/graphql'
+  },
+  body:''
+}
+
+describe('Add todo:', function() {
+
+  var response;
+  before (function(done) {
+    requestContent.body = 'mutation {add (title:"Say hello world") { title }}';
+    request(requestContent, function(error, res, body) {
+      if (!error) {
+        response = res;
+        done();
+      }
+    });
+  });
+
+  it('returns 200', function() {
+    assert.equal(200, response.statusCode);
+  });
+
+  it('returns \'Say hello world\'', function() {
+    assert.equal('Say hello world', JSON.parse(response.body).data.add.pop().title);
+  });
+});
+
+describe('Get todos:', function() {
+
+  var response;
+  before (function(done) {
+    requestContent.body = 'query {todos { title, id }}';
+    request(requestContent, function(error, res, body) {
+      if (!error) {
+        response = res;
+        done();
+      }
+    });
+  });
+
+  it('returns 200', function() {
+    assert.equal(200, response.statusCode);
+  });
+
+  it('returns an array of todos', function() {
+    assert.equal('[object Array]', Object.prototype.toString.apply(JSON.parse(response.body).data.todos));
+  });
+});
