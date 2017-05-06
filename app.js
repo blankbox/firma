75db4da7dfb2d40b49d57e5aa84eadb9baebe90b
David Bebb <davidbebb@gmail.com>
2017-05-06 18:07:51 +0100

    updated body parsing to be neater, and added (very) basic error handlng

--- app.js
+++ app.js
@@ -5,34 +5,37 @@ var logger = require('morgan');
 var {graphql} = require('graphql')
 var bodyParser = require('body-parser');
 
-
 var schema = require ('./graphql/testSchema');
 
 var app = express();
 
 app.use(logger('dev'));
 
-app.use(bodyParser.raw(
+// Get the body for the application/graphql mime-tyepe
+app.use(bodyParser.text(
   { type: 'application/graphql' }
 ));
 
-app.use(function (req, res, next) {
-  req.graphql = {};
-  req.graphql.query = req.body.toString('utf8');
-  next();
-})
-
 app.post('/graphql', function (req, res, next){
-  graphql(schema, req.graphql.query).then(function(result){
-    req.graphql.result = result;
-    next();
+  graphql(schema, req.body).then(function(result){
+    req.result = result;
+    res.send(req.result);
   });
 });
 
+// TODO:Basic error handling - needs work
+app.use(function(req, res, next) {
+  var err = new Error('Not Found');
+  err.status = 404;
+  next(err);
+});
 
-app.use( function (req, res, next) {
-  res.send(req.graphql.result);
-})
+// TODO:Basic error handling - needs work
+app.use(function(err, req, res, next) {
+  // send the  page
+  res.status(err.status || 500);
+  res.send(err.message || 'err');
+});
 
 app.listen(process.env.PORT);
 console.log('Running a GraphQL API server at localhost:' + process.env.PORT+ '/graphql');
